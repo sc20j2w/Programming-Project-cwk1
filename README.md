ba0865e7684baac24293b6102c42509f863eed58
Junzhe Wang <sc20j2w@leeds.ac.uk>
2022-04-08 15:31:41 +0800

    command line operation

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Programming-Project-cwk1
 
-The librarian account:
-
-username: librarian
-
+The librarian account:                                            
+username: librarian                                                    
 password: librarian
+
+main.c: Perform operations on files.                                                                     
+interface.c: The main structure of a program.                                        
+user.c: An interface that users can see and interact with.                      
+save.c: File a program that stores information.                          
+book_management.c: Library management program.
 
 The URL of the repository: https://github.com/sc20j2w/Programming-Project-cwk1.git
 
--- main.c
+++ main.c
@@ -16,47 +16,52 @@ int main(int argc, char** argv){
     Books = (char*)malloc(100*sizeof(char));
     Users = (char*)malloc(100*sizeof(char));
     Book_borrow = (char*)malloc(100*sizeof(char));
-    while(i < argc)
-    {
-        if(t == 0)
-        {
-            strcpy(user_defined,argv[i]);
-            t = 1;
-        }else{
-            strcat(user_defined," ");
-            strcat(user_defined,argv[i]);
-        }
-        if(strstr(user_defined,".txt") == NULL)
-        {
+    if(argv[1] == NULL || argv[2] == NULL || argv[3] == NULL ){
+        printf("You don't name all three files you need. \n");
+        printf("The program automatically generates three files or you can re-enter the program to name the folder. \n");
+        char *a = "users.txt";
+        char *b = "books.txt";
+        char *c = "book_borrow.txt";
+        strcpy(Users,a);
+        printf("The file storing the user is automatically named users. \n");
+        fflush(stdin);
+        strcpy(Books,b);
+        printf("The file storing the book is automatically named books. \n");
+        fflush(stdin);
+        strcpy(Book_borrow,c);
+        printf("The file storing the books that have borrowed is automatically named book_borrow. \n");
+        fflush(stdin);
+    }else{
+        while (i < argc) {
+            if (t == 0) {
+                strcpy(user_defined, argv[i]);
+                t = 1;
+            } else {
+                strcat(user_defined, " ");
+                strcat(user_defined, argv[i]);
+            }
+            if (strstr(user_defined, ".txt") == NULL) {
+                i += 1;
+                continue;
+            }
+            if (j == 1) {
+                strcpy(Users, user_defined);
+            }
+            if (j == 2) {
+                strcpy(Books, user_defined);
+            }
+            if (j == 3) {
+                strcpy(Book_borrow, user_defined);
+            }
+            if (j == 4) {
+                printf("You entered more files than required, only used first three names. \n");
+                printf(" \n");
+                break;
+            }
+            t = 0;
+            j += 1;
             i += 1;
-            continue;
         }
-        if(j == 1)
-        {
-            strcpy(Books,user_defined);
-
-        }
-        if(j == 2)
-        {
-
-            strcpy(Users,user_defined);
-
-        }
-        if(j == 3)
-        {
-
-            strcpy(Book_borrow,user_defined);
-
-        }
-        if(j == 4)
-        {
-            printf("You entered more files than required, only used first three names. \n");
-            printf(" \n");
-            break;
-        }
-        t = 0;
-        j += 1;
-        i += 1;
     }
     printf("Welcome to the library!\n");
     FILE *fp;
